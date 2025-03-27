efc5df441b9b5930ec344d02d5f7c976fc4e6367
Jose Gonzalves <st10194321@vcconnect.edu.za>
2025-03-27 19:17:16 +0200

    TaskManagment app
    styling needed

---
+++ TaskManager/TaskManager/Migrations/20250219093205_initialcreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    TaskID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TaskStatus = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.TaskID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+        }
+    }
+}
---
+++ TaskManager/TaskManager/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.Models
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Details { get; set; }
+        public DateTime DueDate { get; set; }
+        public string TaskStatus { get; set; }
+
+    }
+}
